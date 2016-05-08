74875fb75f6f6d3c6481181b957593f67bd6d2f6
lillfisken <oskar.korall@hotmail.com>
2016-05-08 16:17:47 +0200

    Fixed image size in small listings

--- resources/views/markets/partials/gallery/_baseImage.blade.php
+++ resources/views/markets/partials/gallery/_baseImage.blade.php
@@ -1,3 +1,5 @@
-<a href="{{ route($market->routeBase . '.show', $market->id) }}" class="">
-    <img src="{{ $market->image1_thumb }}" class="gallery-image"/>
-</a>
+<div class="gallery-image-container">
+    <a href="{{ route($market->routeBase . '.show', $market->id) }}" class="">
+        <img src="{{ $market->image1_thumb }}" class="gallery-image"/>
+    </a>
+</div>
